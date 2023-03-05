ae28331b4407c83da55a9e87d0761851d90606d8
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-03-05 23:36:07 +0630

    use generateStaticParams

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -1,12 +1,12 @@
 import { ProductData } from '@/types';
 import Image from 'next/image';
 
-// export async function generateStaticParams() {
-//   const res = await fetch('http://localhost:3000/api/products');
-//   const data: ProductData[] = await res.json();
+export async function generateStaticParams() {
+  const res = await fetch('https://fakestoreapi.com/products');
+  const data: ProductData[] = await res.json();
 
-//   return data.map((product) => ({ slug: product.id.toString() }));
-// }
+  return data.map((product) => ({ slug: product.id.toString() }));
+}
 
 export default async function ProductDetail({
   params: { slug },
