2d046209e3675ed9449e33ba11dcf17f052e7087
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-03-13 00:34:27 +0630

    add and remove functionality done

---
+++ script.js
@@ -0,0 +1,56 @@
+/* add task
+update task
+delete task
+toggle task */
+
+class TodoStore {
+  #todoData = [];
+  updateFunc = null;
+
+  get getTodos() {
+    return this.#todoData;
+  }
+
+  addTodo(taskName) {
+    const newTodo = {
+      id: new Date().getTime().toString(),
+      taskName,
+      done: false,
+    };
+
+    this.#todoData = [newTodo, ...this.#todoData];
+    this.updateFunc && this.updateFunc(this.#todoData);
+  }
+
+  removeTodo(id) {
+    const idxToRemove = this.#todoData.findIndex(
+      (todo) => todo.id === id.toString()
+    );
+
+    if (idxToRemove === -1) {
+      this.updateFunc && this.updateFunc(this.#todoData);
+      return;
+    }
+
+    this.#todoData.splice(idxToRemove, 1);
+    this.updateFunc && this.updateFunc(this.#todoData);
+  }
+
+  subscribe(func) {
+    this.updateFunc = func;
+  }
+}
+
+const todoStore = new TodoStore();
+
+todoStore.subscribe(logger);
+
+function logger(data) {
+  console.log(data);
+}
+
+function updateTodo() {}
+
+function deleteTodo() {}
+
+function toggleTodo() {}
