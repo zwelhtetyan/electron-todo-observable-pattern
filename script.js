64ad452f3dcdb2192855fa9dea87133b75119044
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-03-14 10:25:31 +0630

    add notification

--- script.js
+++ script.js
@@ -27,6 +27,8 @@ class TodoStore {
 
     this.#todoData = [newTodo, ...this.#todoData];
     this.updateFunc && this.updateFunc(this.#todoData);
+
+    notifyMe('Successfully added!', taskName);
   }
 
   updateTodo(id, taskName) {
@@ -139,6 +141,33 @@ function init() {
   todoStore.setTodos = todoData;
 
   updateTodoUI(todoData);
+}
+
+function notifyMe(title, bodyText) {
+  if (!('Notification' in window)) {
+    // Check if the browser supports notifications
+    alert('This browser does not support desktop notification');
+  } else if (Notification.permission === 'granted') {
+    // Check whether notification permissions have already been granted;
+    // if so, create a notification
+
+    const notification = new Notification(title, {
+      body: bodyText,
+    });
+  } else if (Notification.permission !== 'denied') {
+    // We need to ask the user for permission
+    Notification.requestPermission().then((permission) => {
+      // If the user accepts, let's create a notification
+      if (permission === 'granted') {
+        const notification = new Notification(title, {
+          body: bodyText,
+        });
+      }
+    });
+  }
+
+  // At last, if the user has denied notifications, and you
+  // want to be respectful there is no need to bother them anymore.
 }
 
 init();
